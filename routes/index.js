4c948d438d954b4395599531a4a7636ef674e6bd
antoniojcespedes <antjoces@gmail.com>
2015-07-23 20:28:47 +0200

    Creditos

--- routes/index.js
+++ routes/index.js
@@ -8,7 +8,14 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Quiz' });
 });
 
+/*GRT /author */
+router.get('/author', function(req, res){
+	res.render('creditos');
+
+});
+
 router.get('/quizes/question', quizController.question);
 router.get('/quizes/answer', quizController.answer);
+
 
 module.exports = router;
